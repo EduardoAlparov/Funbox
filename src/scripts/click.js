13a73c185bf97d1acf2a63d5004f39de70d33679
Eduard Alparov <workman133@yandex.ru>
2020-08-25 23:32:24 +0300

    fourth upload 25.08.20

--- src/scripts/click.js
+++ src/scripts/click.js
@@ -1,27 +1,35 @@
+const windowWidth = document.documentElement.clientWidth; 
 let menuItem = document.querySelectorAll('.menu__item');
-
-for (var i = 0; i < menuItem.length; i++) {
-  menuItem[i].addEventListener("click", function (event){
-    event.preventDefault();
-    event.currentTarget.classList.toggle("active");
-    event.currentTarget.addEventListener("mouseout", function( event ) {
-      if (event.currentTarget.classList.contains("active") === true) {
-        event.currentTarget.classList.add('activeafter');
-      } else {
-        event.currentTarget.classList.remove('activeafter');
-      };
-      event.currentTarget.addEventListener('click', function(event) {
-        event.currentTarget.classList.remove('activeafter');
-      })
-    });
-  }); 
-};
-
 let blueLink = document.querySelectorAll('.block__desc-link');
 
-for (var i = 0; i < blueLink.length; i++) {
-  blueLink[i].addEventListener("click", function (event){
+if (windowWidth > '480') {
+  for (var i = 0; i < menuItem.length; i++) {
+    menuItem[i].addEventListener("click", function (event){
+      event.preventDefault();
+      event.currentTarget.classList.toggle("active");
+      event.currentTarget.addEventListener("mouseout", function( event ) {
+        if (event.currentTarget.classList.contains("active") === true) {
+          event.currentTarget.classList.add('activeafter');
+        } else {
+          event.currentTarget.classList.remove('activeafter');
+        };
+        event.currentTarget.addEventListener('click', function(event) {
+          event.currentTarget.classList.remove('activeafter');
+        })
+      });
+    }); 
+  };
+
+
+  for (var i = 0; i < blueLink.length; i++) {
+    blueLink[i].addEventListener("click", function (event){
+      event.preventDefault();
+      event.currentTarget.parentElement.classList.add('active');
+    }); 
+  };
+} else { document.addEventListener('touchstart', (e) => {
     event.preventDefault();
-    event.currentTarget.parentElement.classList.add('active');
-  }); 
-};
+    e.currentTarget.classList.toggle("active");
+    console.log(e.touches);
+  });
+}  
